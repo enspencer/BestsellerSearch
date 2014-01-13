eb24b0acaeed1e6fce6365464d34b545021f9041
Emma Spencer <emma.n.spencer@gmail.com>
2014-01-13 16:55:05 -0500

    added bootstrap

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
-  
+
   def index
     @client = Bestsellers::List.new
     @client.configure do |config|  
       config.api_key = ENV['API_KEY']
     end
+    binding.pry
+    @client.single_history()
   end
 
 end
