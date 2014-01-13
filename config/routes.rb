b393cc912ff67fe5f7b8ec296a18c307cdd8034b
Emma Spencer <emma.n.spencer@gmail.com>
2014-01-13 15:40:43 -0500

    created Lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BestsellerSearch::Application.routes.draw do
+  get "lists/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
